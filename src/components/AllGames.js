307efd8fcb50821fc9900df22cb1d7202be2946b
Alex Brian Naughton <alexbriannaughton@gmail.com>
2023-01-26 12:20:51 -0800

    poss horns at new goals, will check if it works at game time

--- src/components/AllGames.js
+++ src/components/AllGames.js
@@ -1,6 +1,6 @@
 import OneGame from "./OneGame"
 import { useState, useEffect } from "react"
-
+import useSound from "use-sound";
 
 function AllGames({ horns, logos, setCurrentPage }) {
 
@@ -9,9 +9,78 @@ function AllGames({ horns, logos, setCurrentPage }) {
 
     setCurrentPage("home")
 
-    let goalsObject = {}
+    // prepare all of the buzzers... couldnt think of a cleaner way to do this...
 
-    // intial fetch -- no buzzer sounds
+    const [ANAhorn] = useSound(horns.ANA)
+    const [ARIhorn] = useSound(horns.ARI)
+    const [BOShorn] = useSound(horns.BOS)
+    const [BUFhorn] = useSound(horns.BUF)
+    const [CARhorn] = useSound(horns.CAR)
+    const [CBJhorn] = useSound(horns.CBJ)
+    const [CGYhorn] = useSound(horns.CGY)
+    const [CHIhorn] = useSound(horns.CHI)
+    const [COLhorn] = useSound(horns.COL)
+    const [DALhorn] = useSound(horns.DAL)
+    const [DEThorn] = useSound(horns.DET)
+    const [EDMhorn] = useSound(horns.EDM)
+    const [FLAhorn] = useSound(horns.FLA)
+    const [LAKhorn] = useSound(horns.LAK)
+    const [MINhorn] = useSound(horns.MIN)
+    const [MTLhorn] = useSound(horns.MTL)
+    const [NJDhorn] = useSound(horns.NJD)
+    const [NSHhorn] = useSound(horns.NSH)
+    const [NYIhorn] = useSound(horns.NYI)
+    const [NYRhorn] = useSound(horns.NYR)
+    const [OTThorn] = useSound(horns.OTT)
+    const [PHIhorn] = useSound(horns.PHI)
+    const [PIThorn] = useSound(horns.PIT)
+    const [SEAhorn] = useSound(horns.SEA)
+    const [SJShorn] = useSound(horns.SJS)
+    const [STLhorn] = useSound(horns.STL)
+    const [TBLhorn] = useSound(horns.TBL)
+    const [TORhorn] = useSound(horns.TOR)
+    const [VANhorn] = useSound(horns.VAN)
+    const [VGKhorn] = useSound(horns.VGK)
+    const [WPGhorn] = useSound(horns.WPG)
+    const [WSHhorn] = useSound(horns.WSH)
+
+    function soundTeamHorn(abb) {
+        if (abb === 'ANA') return ANAhorn()
+        if (abb === 'ARI') return ARIhorn()
+        if (abb === 'BOS') return BOShorn()
+        if (abb === 'BUF') return BUFhorn()
+        if (abb === 'CAR') return CARhorn()
+        if (abb === 'CBJ') return CBJhorn()
+        if (abb === 'CGY') return CGYhorn()
+        if (abb === 'CHI') return CHIhorn()
+        if (abb === 'COL') return COLhorn()
+        if (abb === 'DAL') return DALhorn()
+        if (abb === 'DET') return DEThorn()
+        if (abb === 'EDM') return EDMhorn()
+        if (abb === 'FLA') return FLAhorn()
+        if (abb === 'LAK') return LAKhorn()
+        if (abb === 'MIN') return MINhorn()
+        if (abb === 'MTL') return MTLhorn()
+        if (abb === 'NJD') return NJDhorn()
+        if (abb === 'NSH') return NSHhorn()
+        if (abb === 'NYI') return NYIhorn()
+        if (abb === 'NYR') return NYRhorn()
+        if (abb === 'OTT') return OTThorn()
+        if (abb === 'PHI') return PHIhorn()
+        if (abb === 'PIT') return PIThorn()
+        if (abb === 'SEA') return SEAhorn()
+        if (abb === 'SJS') return SJShorn()
+        if (abb === 'STL') return STLhorn()
+        if (abb === 'TBL') return TBLhorn()
+        if (abb === 'TOR') return TORhorn()
+        if (abb === 'VAN') return VANhorn()
+        if (abb === 'VGK') return VGKhorn()
+        if (abb === 'WPG') return WPGhorn()
+        if (abb === 'WSH') return WSHhorn()
+    }
+
+    // intial fetch -- no buzzer sounds -- set up an object of all the current goals, so we know when to toot a horn
+    let goalsObject = {}
     useEffect(() => {
         fetch("https://nhl-score-api.herokuapp.com/api/scores/latest").then((r) => {
             if (r.ok) {
@@ -20,12 +89,11 @@ function AllGames({ horns, logos, setCurrentPage }) {
                     setScoresLoaded(true)
 
                     scores.games.map((game) => {
-                        
+                        // create goals object to check if there are new goals
                         goalsObject[Object.keys(game.scores)[0]] = Object.values(game.scores)[0]
                         goalsObject[Object.keys(game.scores)[1]] = Object.values(game.scores)[1]
+                        console.log(Object.keys(game.scores)[0])
                     })
-                    console.log(goalsObject)
-                    
                     console.log('initial fetch!')
                 })
             }
@@ -44,32 +112,33 @@ function AllGames({ horns, logos, setCurrentPage }) {
             if (r.ok) {
                 r.json().then((scores) => {
 
-                    setScores(scores)            
+                    setScores(scores)
                     console.log("additional fetch!")
 
                     let updateObject = {}
 
-                    scores.games.map((game) => {                      
+                    scores.games.map((game) => {
                         updateObject[Object.keys(game.scores)[0]] = Object.values(game.scores)[0]
                         updateObject[Object.keys(game.scores)[1]] = Object.values(game.scores)[1]
                     })
 
-                    let count = 0
+                    let teamIdx = 0
 
-                    while (count < Object.keys(updateObject).length) {
-                        if (Object.values(updateObject)[count] !== Object.values(goalsObject)[count]) {
-                            console.log(Object.keys(updateObject)[count], "scored goal number: ", Object.values(updateObject)[count], "@", Date.now())
+                    while (teamIdx < Object.keys(updateObject).length) {
+                        // if the new goals object (updateObject) does not equal the goalsObject that means somebody scored, and we need to toot a horn
+                        if (Object.values(updateObject)[teamIdx] !== Object.values(goalsObject)[teamIdx]) {
+
+                            console.log(Object.keys(updateObject)[teamIdx], "scored goal number: ", Object.values(updateObject)[teamIdx], "@", Date.now())
+
                             goalsObject = updateObject
+
+                            soundTeamHorn(Object.keys(updateObject)[teamIdx])
+
                         } else {
-                            console.log("nobody scored")
+                            console.log(Object.keys(updateObject)[teamIdx], "didn't score since last check")
                         }
-
-                        count = count + 1
+                        teamIdx = teamIdx + 1
                     }
-
-                    // console.log(Object.keys(updateObject).length)
-                    // console.log(goalsObject)
-                    
                 })
             }
         })
